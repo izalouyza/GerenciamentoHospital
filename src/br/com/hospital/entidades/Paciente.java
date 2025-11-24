5d9369c05e887282bf3ef0f820e8de74448a006e
livianlucena <livianmarialucena@gmail.com>
2025-11-24 15:52:21 -0300

    Atualizando o construtor após a adição do atributo endereco na classe mãe (Pessoa) e removendo os atributos temperatura, oxigencao e pressaoArterial, pois como se trata de um agendamento, são dados desnecessários

--- src/br/com/hospital/entidades/Paciente.java
+++ src/br/com/hospital/entidades/Paciente.java
@@ -3,17 +3,11 @@ package br.com.hospital.entidades;
 public class Paciente extends Pessoa {
     private int idade;
     private String historicoClinico;
-    private double temperatura;
-    private String pressaoArterial;
-    private int oxigenacao;
 
-    public Paciente(int id, String nome, String cpf, String telefone, String email, int idade, String historicoClinico,  double temperatura, String pressaoArterial, int oxigenacao) {
-        super(id, nome, cpf, telefone, email);
+    public Paciente(int id, String nome, String cpf, String telefone, String email, String endereco, int idade, String historicoClinico) {
+        super(id, nome, cpf, telefone, email, endereco);
         this.idade = idade;
         this.historicoClinico = historicoClinico;
-        this.temperatura = temperatura;
-        this.pressaoArterial = pressaoArterial;
-        this.oxigenacao = oxigenacao;
     }
 
     @Override
@@ -23,11 +17,8 @@ public class Paciente extends Pessoa {
                 
                 Dados clínicos:
                 Idade: %d
-                Temperatura: %.2f
-                Pressão Arterial: %s
-                Oxigenação: %d%%
                 Histórico Clínico: %s
-                """, getIdade(), getTemperatura(), getPressaoArterial(), getOxigenacao(), getHistoricoClinico());
+                """, getIdade(), getHistoricoClinico());
     }
 
     public int getIdade() {
@@ -46,23 +37,4 @@ public class Paciente extends Pessoa {
         this.historicoClinico = historicoClinico;
     }
 
-    public double getTemperatura() {
-        return temperatura;
-    }
-
-    public void setTemperatura(double temperatura) {
-        this.temperatura = temperatura;
-    }
-
-    public String getPressaoArterial() {
-        return pressaoArterial;
-    }
-
-    public void setPressaoArterial(String pressaoArterial) {
-        this.pressaoArterial = pressaoArterial;
-    }
-
-    public int getOxigenacao() {
-        return oxigenacao;
-    }
 }
