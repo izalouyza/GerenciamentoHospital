74d541107620feeebe6ff85bc440ed7c807320a7
LeonardoA.S.Sousa <leonardoassousati@gmail.com>
2025-11-26 22:42:36 -0300

    Implementação básica da função adicionar e listar

--- src/br/com/hospital/sistema/Hospital.java
+++ src/br/com/hospital/sistema/Hospital.java
@@ -1,38 +1,41 @@
 package br.com.hospital.sistema;
 
-import br.com.hospital.entidades.Medico;
-import br.com.hospital.entidades.Paciente;
+import br.com.hospital.entidades.Pessoa;
 import br.com.hospital.interfaces.Gerenciavel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Hospital implements Gerenciavel {
-    private List<Medico> medicosRegistrados;
-    private List<Paciente> pacientesRegistrados;
+public class Hospital implements Gerenciavel<Pessoa> {
+    final private List<Pessoa> pessoasRegistradas;
 
     public Hospital (){
-        this.medicosRegistrados = new ArrayList<>();
-        this.pacientesRegistrados = new ArrayList<>();
+        this.pessoasRegistradas = new ArrayList<>();
     }
 
     @Override
-    public void adicionar(Object elemento) {
-
+    public void adicionar(Pessoa elemento) {
+        pessoasRegistradas.add(elemento);
     }
 
     @Override
     public void listar() {
-
+        if (pessoasRegistradas.isEmpty()) {
+            System.out.println("Nenhuma pessoa registrada, não é possível listar.");
+            return;
+        }
+        for (Pessoa pessoaListar: pessoasRegistradas){
+            pessoaListar.exibirInformacoes();
+        }
     }
 
     @Override
-    public Object buscar(String identificador) {
+    public Pessoa buscar(String identificador) {
         return null;
     }
 
     @Override
-    public boolean editar(String identificador, Object novoElemento) {
+    public boolean editar(String identificador, Pessoa novoElemento) {
         return false;
     }
 
